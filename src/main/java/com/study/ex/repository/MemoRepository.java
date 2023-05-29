8369f6e38c775f86270fbba3744679ab35c851a6
song-inhyo <izp1012@naver.com>
2023-05-29 13:16:34 +0900

    쿼리메소드 적용 전 커밋

--- src/main/java/com/study/ex/repository/MemoRepository.java
+++ src/main/java/com/study/ex/repository/MemoRepository.java
@@ -1,7 +1,18 @@
 package com.study.ex.repository;
 
 import com.study.ex.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo,Long> {
+
+    //Query 방식 사용전 - 메소드 이름이 상당히 길어짐
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+    //ㅡmno 가 10보다 작은 데이터를 삭제
+    void deleteMemoByMnoLessThan(Long num);
 }
--- src/test/java/com/study/ex/repository/MemoRepositoryTests.java
+++ src/test/java/com/study/ex/repository/MemoRepositoryTests.java
@@ -6,9 +6,15 @@ import net.bytebuddy.description.NamedElement;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 import javax.swing.text.html.Option;
 import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -93,4 +99,119 @@ public class MemoRepositoryTests {
         Long mno = 99L;
         memoRepository.deleteById(mno);
     }
+
+    ////////////////////////////////////////////////////페이징 처리////////////////////////////////////////////////////
+    @Test
+    public void testPagingDefault(){
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testPageDefault(){
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        System.out.println("==========================================================");
+
+        System.out.println("Total page : "+result.getTotalPages()); //총 몇 페이지
+
+        System.out.println("Total Count : "+result.getTotalElements()); //전체 개수
+
+        System.out.println("Total Number : "+result.getNumber());//현재 페이지 번호 0부터 시작
+
+        System.out.println("Total Size : "+result.getSize());//페이지당 데이터 개수
+
+        System.out.println("has next Page ? : "+result.hasNext());//다음 페이지 존재 여부
+
+        System.out.println("first Page ? : "+result.isFirst());//시작 페이지(0) 여부
+
+        System.out.println("==========================================================");
+        for (Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    }
+    //페이징에 정렬 조건 추가하기
+    //Sort 라는 파라미터를 통해 정렬 처리 다중 필드 및 오름차순, 내림차순이 가능하다
+    @Test
+    public void testSort(){
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sort3 = sort1.and(sort2);
+
+        Pageable pageable1 = PageRequest.of(0,10, sort1);
+        Pageable pageable2 = PageRequest.of(0,30, sort3);
+
+        Page<Memo> result1 = memoRepository.findAll(pageable1);
+        Page<Memo> result2 = memoRepository.findAll(pageable2);
+
+            System.out.println("Result1 = ");
+        result1.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+
+            System.out.println("Result2 = ");
+        result2.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+
+    //////////////////////////////////////////JPQL Query//////////////////////////////////////////
+    //Spring Data JPA 에서 제공하는 방법들
+    //쿼리 메서드  : 메서드의이름 자체가 쿼리의 구문으로 처리되는 기능  <<이거랑
+    //@Query : SQL 과 유사하게 엔티티 클래스의 정보를 이용하여 쿼리를 작성하는 기능  <<이거 테스트
+    //Querydsl 의 동작기능
+
+
+
+    // 예제 1 : mno 가 70부터 80인 객체를 구하고 mno 의 역순으로 정렬하여라
+    //(Query 메서드 적용 전)
+    @Test
+    public void testQueryMethod(){
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L,80L);
+
+        for(Memo memo : list){
+            System.out.println(memo);
+        }
+    }
+
+
+    @Test
+    public void testQueryMethodWithPageble(){
+        //Paging 처리 적용 10개를 출력할 거다 (PageRequest) mno 의 역순으로
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        //Mno 를 기준(10~50 객체) 으로 가져올건데 Paging 을 적용해서.
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+        //-> 50번 ~40번까지 출력됨
+        result.get().forEach(memo -> System.out.println(memo));
+
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethod(){
+        /*
+        @Transactional
+        Delete 의 경우애는 Commit, Transactional 어노테이션이 사용되어야 하는데
+        deleteBy 인 경우에는 우선은 'select 문으로 해당 엔티티 객체들을 가져오는 작업과 각 에티티를 삭제하는 작업이 같이 이루어지기 떄문'
+
+        Transactional 이 없는 경우 발생하는 오류
+        No EntityManager with actual transaction available for current thread - cannot reliably process 'remove' call
+
+        @Commit
+        최종결과를 커밋하기 위해서 사용하는데, 이를 적용하지 않은면 기본적으로 deleteBy 는 롤백 처리되어 받영되지 않기떄문에
+        Delete 메소드의 경우에는 Commit 을 적용해야한다   -> Delete 는 실제 개발에선 잘 사용 X (Why ? 한번에 삭제가아니라 각 엔티티 객체를 하나씩 삭제하기 때문에)
+
+         */
+        memoRepository.deleteMemoByMnoLessThan(10L);
+    }
+
+
+
 }
